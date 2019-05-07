7c7fb58fbdba16770e76c88b79c57ca420d27001
Turner Schumann <turnerschumann@gmail.com>
2019-05-07 00:47:00 +0000

    Create function to to Print Avatar URLs

--- download_avatars.js
+++ download_avatars.js
@@ -8,11 +8,25 @@ function getRepoContributors(repoOwner, repoName, cb) {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request',
-      'Authorization': 'token'
+      'Authorization': 'Token '+ token
     }
   };
 
+
   request(options, function(err, res, body) {
-    cb(err, body);
+    var parsedBody = JSON.parse(body);
+    cb(err, parsedBody);
+
+    for (var i = 0; i < parsedBody.length; i++) {
+      console.log("Avatar URL: " + parsedBody[i].avatar_url);
+    }
   });
 }
+
+
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  // console.log("Result:", result);
+  // console.log(result[0].avatar_url);
+});
